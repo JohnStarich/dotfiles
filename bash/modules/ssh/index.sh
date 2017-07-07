1480939cc02b8c1a2bb28fe602d261d837c448e5
John Starich <johnstarich@johnstarich.com>
2017-07-07 10:33:30 -0500

    Fix sed bug on James' machine

--- bash/modules/ssh/index.sh
+++ bash/modules/ssh/index.sh
@@ -6,5 +6,5 @@ if [ ! -f ~/.ssh/known_hosts ]; then
 fi
 
 if [[ "$SHELL" == *bash* ]]; then
-	complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
+	complete -W "$(echo $(cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed 's/,.*//g' | uniq | grep -v "\["))" ssh
 fi
