cb7576cd622fe1bac56d6c6f264b410c9b4ef78f
John Starich <johnstarich@johnstarich.com>
2018-11-11 14:24:23 -0600

    Fix ssh completion failure mode when known_hosts does not exist

--- bash/modules/ssh/index.sh
+++ bash/modules/ssh/index.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ ! -f ~/.ssh/known_hosts ]; then
-    echo '~/.ssh/known_hosts does not exist'
-    return 1
-fi
-
 if [[ "$SHELL" == *bash* ]]; then
-	complete -W "$(echo $(cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed 's/,.*//g' | uniq | grep -v "\["))" ssh
+	complete -W "$([[ -e ~/.ssh/known_hosts ]] && echo $(cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed 's/,.*//g' | uniq | grep -v "\["))" ssh
 fi
