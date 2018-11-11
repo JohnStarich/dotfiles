841874b1a2c4d89896076a0e3e6ae3076ff38779
John Starich <johnstarich@johnstarich.com>
2018-11-11 16:52:11 -0600

    Fix kubectl prompt when KUBECONFIG file may not exist

--- zsh/bundles/johnstarich/zsh-kubectl-prompt/zsh-kubectl-prompt.plugin.zsh
+++ zsh/bundles/johnstarich/zsh-kubectl-prompt/zsh-kubectl-prompt.plugin.zsh
@@ -7,7 +7,7 @@ function js_kubectl_prompt() {
     local kube_color="%{$fg_bold[magenta]%}"
     local context_color="%{$fg_bold[red]%}"
     local reset_color="%{$reset_color%}"
-    if [[ ! -z "$KUBECONFIG" ]]; then
+    if [[ -n "$KUBECONFIG" && -e "$KUBECONFIG" ]]; then
         if [[ "$_kubectl_prompt_oldconfig" != "$KUBECONFIG" ]]; then
             _kubectl_prompt_context=$(kubectl config current-context)
             _kubectl_prompt_oldconfig=$KUBECONFIG
