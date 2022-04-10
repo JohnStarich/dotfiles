b30498e869a208d54ec522e8da43c02bb3af8226
John Starich <johnstarich@johnstarich.com>
2022-04-10 16:12:25 -0500

    Add docker config context name to prompt

--- zsh/bundles/johnstarich/zsh-docker-prompt/zsh-docker-prompt.plugin.zsh
+++ zsh/bundles/johnstarich/zsh-docker-prompt/zsh-docker-prompt.plugin.zsh
@@ -2,8 +2,18 @@
 
 autoload -Uz colors && colors
 
+function js_docker_prompt_text() {
+    print "%{${fg[green]}%}docker:(%{${fg[yellow]}%}$*%{${fg[green]}%})%{$reset_color%} "
+}
+
 function js_docker_prompt() {
+    if [[ -f ~/.docker/config.json ]]; then
+        contextName=$(jq -r .currentContext ~/.docker/config.json)
+        if [[ -n "$contextName" && "$contextName" != null ]]; then
+            js_docker_prompt_text "$contextName"
+        fi
+    fi
     if [[ -n "$DOCKER_MACHINE_NAME" ]]; then
-        print "%{${fg[green]}%}docker:(%{${fg[yellow]}%}$DOCKER_MACHINE_NAME%{${fg[green]}%})%{$reset_color%} "
+        js_docker_prompt_text "$DOCKER_MACHINE_NAME"
     fi
 }
