e0478f80b19e79d3d56b3a141f788d60aa986e77
John Starich <johnstarich@johnstarich.com>
2022-11-23 16:06:01 -0600

    apt: Remove things I no longer use

--- apt/install.sh
+++ apt/install.sh
@@ -5,40 +5,27 @@ if ! which apt-get >/dev/null; then
 fi
 
 apt_repos=(
-    ppa:masterminds/glide
 )
 apt_packages=(
     bash-completion
     cmake
     coreutils
-    ctags
     dos2unix
     git
-    glide
     golang
     htop
     jq
-    maven
-    mongodb
     mosh
     ncdu
     neovim
-    nodejs
-    pandoc
-    python
     python3
-    ruby
-    sbt
-    scala
     silversearcher-ag
     thefuck
-    tmate
     tmux
     trash-cli
     vim
     watch
     wget
-    openjdk-8-jdk
 )
 
 shopt -s nullglob
@@ -58,11 +45,6 @@ if is_installed "${apt_packages[@]}"; then
         installed=\$(dpkg-query --show --showformat='\${db:Status-Status}\n' "\$@" 2>&1 | grep -v '^installed$' | wc -l)
         [[ \$installed -ne 0 ]]
     }
-    # sbt
-    if [[ ! -f /etc/apt/sources.list.d/sbt.list ]]; then
-        echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-        apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-    fi
 
     apt-get update
     for repo in ${apt_repos[@]}; do
