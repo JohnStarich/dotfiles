58a80f3af7d1bbc3887c493b118afed14b2fe90e
John Starich <johnstarich@johnstarich.com>
2022-11-26 01:48:34 -0600

    zsh: Extract both git status checks

--- zsh/bundles/johnstarich/zsh-git-prompt/zsh-git-prompt.plugin.zsh
+++ zsh/bundles/johnstarich/zsh-git-prompt/zsh-git-prompt.plugin.zsh
@@ -1,5 +1,20 @@
 #!/bin/zsh
 
+function js_git_prompt_ternary_string() {
+    local success=true
+    local failure=false
+    if [[ "$1" == '!' ]]; then
+        shift
+        success=false
+        failure=true
+    fi
+    if "$@" >&- 2>&-; then
+        echo "$success"
+    else
+        echo "$failure"
+    fi
+}
+
 autoload -Uz colors && colors
 js_ret_prompt="%(?:%{${fg[green]}%}➜:%{${fg[red]}%}➜)"
 function js_git_prompt() {
@@ -7,7 +22,10 @@ function js_git_prompt() {
     ref=$(git symbolic-ref HEAD 2> /dev/null) || ref=$(git rev-parse --short HEAD 2> /dev/null) || return 0
     ref=${ref#refs/heads/}
     echo -n "%{${fg[blue]}%}git:(%{${fg[red]}%}$ref%{${fg[blue]}%})%{$reset_color%} "
-    if ! git diff --no-ext-diff --quiet --exit-code || git ls-files --others --exclude-standard --error-unmatch . >&- 2>&-; then
+
+    local has_modifications=$(js_git_prompt_ternary_string ! git diff --no-ext-diff --quiet --exit-code)
+    local has_untracked=$(js_git_prompt_ternary_string git ls-files --others --exclude-standard --error-unmatch .)
+    if [[ "$has_modifications" == true || "$has_untracked" == true ]]; then
         echo -n "%{${fg[yellow]}%}✗%{$reset_color%} "
     fi
 }
