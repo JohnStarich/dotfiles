0babff6619d7ddb2ecead861639386bba97eeaf3
John Starich <johnstarich@johnstarich.com>
2022-11-26 19:08:16 -0600

    zsh: Use hooks to cache git prompt vars

--- zsh/bundles/johnstarich/zsh-git-prompt/zsh-git-prompt.plugin.zsh
+++ zsh/bundles/johnstarich/zsh-git-prompt/zsh-git-prompt.plugin.zsh
@@ -1,38 +1,33 @@
 #!/bin/zsh
 
-function js_git_prompt_ternary_string() {
-    local success=true
-    local failure=false
-    if [[ "$1" == '!' ]]; then
-        shift
-        success=false
-        failure=true
-    fi
-    if "$@" >&- 2>&-; then
-        echo "$success"
-    else
-        echo "$failure"
-    fi
-}
-
 autoload -Uz colors && colors
-js_ret_prompt="%(?:%{${fg[green]}%}➜:%{${fg[red]}%}➜)"
-function js_git_prompt() {
-    local ref
-    ref=$(git symbolic-ref HEAD 2> /dev/null) || ref=$(git rev-parse --short HEAD 2> /dev/null) || return 0
-    ref=${ref#refs/heads/}
-    echo -n "%{${fg[blue]}%}git:(%{${fg[red]}%}$ref%{${fg[blue]}%})%{$reset_color%} "
 
-    local has_modifications=$(js_git_prompt_ternary_string ! git diff --no-ext-diff --quiet --exit-code)
-    local has_untracked=$(js_git_prompt_ternary_string git ls-files --others --exclude-standard --error-unmatch .)
-    if [[ "$has_modifications" == true || "$has_untracked" == true ]]; then
-        if [[ "$has_modifications" == true ]]; then
-            echo -n "%{${fg[yellow]}%}"
-            printf "✗"
-        else
-            echo -n "%{${fg[green]}%}"
-            printf "✔"
-        fi
-        echo "%{$reset_color%} "
+js_git_prompt=''
+js_git_prev_dir=''
+js_git_cache_main_ref=''
+function js_git_gen_prompt() {
+    local current_git_dir
+    if ! current_git_dir=$(git rev-parse --show-toplevel --quiet 2>&-); then
+        js_git_prompt=''
+        return 0
     fi
+    if [[ "$js_git_prev_dir" != "$current_git_dir" || "$js_git_cache_main_ref" == '' ]]; then
+        js_git_cache_main_ref=$(git symbolic-ref HEAD 2> /dev/null) || js_git_cache_main_ref=$(git rev-parse --short HEAD 2> /dev/null) || true
+        js_git_cache_main_ref=${js_git_cache_main_ref#refs/heads/}
+        js_git_prev_dir=$current_git_dir
+    fi
+
+    js_git_prompt="%{${fg[blue]}%}git:(%{${fg[red]}%}$js_git_cache_main_ref%{${fg[blue]}%})"
+
+    if ! git diff --no-ext-diff --quiet --exit-code >&- 2>&-; then
+        # Has modifications
+        js_git_prompt+=" %{${fg[yellow]}%}✗"
+    elif git ls-files --others --exclude-standard --error-unmatch . >&- 2>&-; then
+        # Has untracked files
+        js_git_prompt+=" %{${fg[green]}%}✔"
+    fi
+    js_git_prompt+="%{$reset_color%} "
 }
+
+autoload -Uz add-zsh-hook 
+add-zsh-hook precmd js_git_gen_prompt
