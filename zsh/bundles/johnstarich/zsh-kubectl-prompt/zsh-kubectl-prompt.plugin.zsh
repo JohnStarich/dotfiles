51af5a977f8bf34930a3fd16290878cddf25f219
John Starich <johnstarich@johnstarich.com>
2022-11-26 22:35:36 -0600

    zsh: Improve kubectl prompt cache handling

--- zsh/bundles/johnstarich/zsh-kubectl-prompt/zsh-kubectl-prompt.plugin.zsh
+++ zsh/bundles/johnstarich/zsh-kubectl-prompt/zsh-kubectl-prompt.plugin.zsh
@@ -1,20 +1,29 @@
 #!/bin/zsh
 
-_kubectl_prompt_oldconfig=''
-_kubectl_prompt_context=''
+autoload -Uz colors && colors
+zmodload zsh/stat
 
-function js_kubectl_prompt() {
+js_kubectl_prompt=''
+declare -i js_kubectl_mtime=0
+
+function js_kubectl_gen_prompt() {
     local kube_color="%{$fg_bold[magenta]%}"
     local context_color="%{$fg_bold[red]%}"
     local reset_color="%{$reset_color%}"
-    if [[ -n "$KUBECONFIG" && -e "$KUBECONFIG" ]]; then
-        if [[ "$_kubectl_prompt_oldconfig" != "$KUBECONFIG" ]]; then
-            _kubectl_prompt_context=$(kubectl config current-context)
-            _kubectl_prompt_oldconfig=$KUBECONFIG
-        fi
-        print "${kube_color}kube:(${context_color}${_kubectl_prompt_context}${kube_color})${reset_color} "
+    if [[ -z "$KUBECONFIG" || ! -e "$KUBECONFIG" ]]; then
+        js_kubectl_prompt=''
+        js_kubectl_mtime=0
+        return 0
+    fi
+    declare -i current_mtime=$(stat +mtime "$KUBECONFIG" 2>&- || echo 0)
+    if (( js_kubectl_mtime < current_mtime )); then
+        js_kubectl_mtime=$current_mtime
+        js_kubectl_prompt="${kube_color}kube:(${context_color}$(kubectl config current-context)${kube_color})${reset_color} "
     fi
 }
+
+autoload -Uz add-zsh-hook 
+add-zsh-hook precmd js_kubectl_gen_prompt
 
 function js_kubectl_toggle() {
     if [[ -n "$KUBECONFIG" ]]; then
