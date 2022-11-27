0f0cf7c4bd14648ca99c64848094752ed9677634
John Starich <johnstarich@johnstarich.com>
2022-11-26 23:15:57 -0600

    zsh: Improve docker prompt cache handling

--- zsh/bundles/johnstarich/zsh-docker-prompt/zsh-docker-prompt.plugin.zsh
+++ zsh/bundles/johnstarich/zsh-docker-prompt/zsh-docker-prompt.plugin.zsh
@@ -1,19 +1,36 @@
 #!/bin/zsh
 
 autoload -Uz colors && colors
+zmodload zsh/stat
+
+js_docker_prompt=''
+declare -i js_docker_mtime=0
+
+function js_docker_gen_prompt() {
+    if [[ -n "$DOCKER_MACHINE_NAME" ]]; then
+        js_docker_prompt=$(js_docker_prompt_text "$DOCKER_MACHINE_NAME")
+        return 0
+    fi
+    local docker_config=~/.docker/config.json
+    if [[ ! -f "$docker_config" ]]; then
+        js_docker_prompt=''
+        return 0
+    fi
+    declare -i current_mtime=$(stat +mtime "$docker_config" 2>&- || echo 0)
+    if (( js_docker_mtime >= current_mtime )); then
+        return 0
+    fi
+    js_docker_mtime=$current_mtime
+
+    local context_name=$(jq -r .currentContext "$docker_config")
+    if [[ -n "$context_name" && "$context_name" != null ]]; then
+        js_docker_prompt=$(js_docker_prompt_text "$context_name")
+    fi
+}
+
+autoload -Uz add-zsh-hook 
+add-zsh-hook precmd js_docker_gen_prompt
 
 function js_docker_prompt_text() {
     print "%{${fg[green]}%}docker:(%{${fg[yellow]}%}$*%{${fg[green]}%})%{$reset_color%} "
-}
-
-function js_docker_prompt() {
-    if [[ -f ~/.docker/config.json ]]; then
-        contextName=$(jq -r .currentContext ~/.docker/config.json)
-        if [[ -n "$contextName" && "$contextName" != null ]]; then
-            js_docker_prompt_text "$contextName"
-        fi
-    fi
-    if [[ -n "$DOCKER_MACHINE_NAME" ]]; then
-        js_docker_prompt_text "$DOCKER_MACHINE_NAME"
-    fi
 }
