f8135fa35e375c3be88624221dc73ef354953665
John Starich <johnstarich@johnstarich.com>
2022-11-30 23:45:48 -0600

    httpdir: Open in browser

--- go/httpdir/main.go
+++ go/httpdir/main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"os/exec"
 )
 
 func main() {
-	dir := "."
-	if len(os.Args) >= 2 {
-		dir = os.Args[1]
-	}
-	listener, err := net.Listen("tcp", ":0")
+	open := flag.Bool("open", false, "On systems with an 'open' command, runs 'open $SERVER_URL'")
+	dir := flag.String("path", ".", "The directory to serve via HTTP. Defaults to current directory.")
+	flag.Parse()
+
+	listener, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Starting server:", listener.Addr().String())
-	http.Serve(listener, http.FileServer(http.Dir(dir)))
+	addr := listener.Addr().String()
+	fmt.Println("Running server:", addr)
+	if *open {
+		go openHost(addr)
+	}
+	http.Serve(listener, http.FileServer(http.Dir(*dir)))
+}
+
+func openHost(addr string) {
+	url := url.URL{
+		Scheme: "http",
+		Host:   addr,
+	}
+	cmd := exec.Command("open", url.String())
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
 }
