d5d1ff191a484e36a9f16509285a8565ec1c4c60
John Starich <johnstarich@johnstarich.com>
2022-12-11 20:13:28 -0600

    httpdir: Add base path flag

--- go/httpdir/main.go
+++ go/httpdir/main.go
@@ -13,6 +13,7 @@ import (
 func main() {
 	open := flag.Bool("open", false, "On systems with an 'open' command, runs 'open $SERVER_URL'")
 	dir := flag.String("path", ".", "The directory to serve via HTTP. Defaults to current directory.")
+	basePath := flag.String("base", "/", "The base URL to use. All paths must use this prefix.")
 	flag.Parse()
 
 	listener, err := net.Listen("tcp", "localhost:0")
@@ -20,17 +21,21 @@ func main() {
 		panic(err)
 	}
 	addr := listener.Addr().String()
-	fmt.Println("Running server:", addr)
+	fmt.Println("Running server:", addr, *basePath)
 	if *open {
-		go openHost(addr)
+		go openHost(addr, *basePath)
 	}
-	http.Serve(listener, http.FileServer(http.Dir(*dir)))
+	mux := http.NewServeMux()
+	handler := http.FileServer(http.Dir(*dir))
+	mux.Handle(*basePath+"/", http.StripPrefix(*basePath, handler))
+	http.Serve(listener, mux)
 }
 
-func openHost(addr string) {
+func openHost(addr, basePath string) {
 	url := url.URL{
 		Scheme: "http",
 		Host:   addr,
+		Path:   basePath,
 	}
 	cmd := exec.Command("open", url.String())
 	cmd.Stdout = os.Stdout
