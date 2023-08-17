60197d06eb9dc47ef4bb7a3d06362f135034d629
John Starich <johnstarich@johnstarich.com>
2023-08-17 15:23:14 -0500

    Add json2yaml script

---
+++ go/json2yaml/go.mod
@@ -0,0 +1,5 @@
+module github.com/johnstarich/go/json2yaml
+
+go 1.19
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
--- go/json2yaml/main.go
+++ go/json2yaml/main.go
@@ -18,11 +18,11 @@ func main() {
 		in = file
 	}
 	var value any
-	err := yaml.NewDecoder(in).Decode(&value)
+	err := json.NewDecoder(in).Decode(&value)
 	if err != nil {
 		panic(err)
 	}
-	err = json.NewEncoder(os.Stdout).Encode(value)
+	err = yaml.NewEncoder(os.Stdout).Encode(value)
 	if err != nil {
 		panic(err)
 	}
