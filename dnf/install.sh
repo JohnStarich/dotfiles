214a1254553cd4eee0bcbb98d761cd0501c516f3
John Starich <johnstarich@johnstarich.com>
2023-10-18 01:01:21 -0700

    dnf: Add docker

--- dnf/install.sh
+++ dnf/install.sh
@@ -4,18 +4,42 @@ if ! which dnf >/dev/null 2>/dev/null; then
     exit 0
 fi
 
+installed_packages=$(dnf list --installed)
+function is_installed() {
+    local pkg=$1
+    [[ "$installed_packages" =~ (^|$'\n')"$pkg"\. ]]
+}
+function ensure_installed() {
+    local packages=("$@")
+    for pkg in "${packages[@]}"; do
+        if ! is_installed "$pkg"; then
+            echo "Package $pkg not installed. Running bulk install..."
+            sudo dnf install -y "${packages[@]}"
+            return
+        fi
+    done
+}
+function ensure_repo() {
+    local name=$1
+    local url=$2
+    if [[ ! -f "/etc/yum.repos.d/$name" ]]; then
+        sudo dnf config-manager --add-repo "$repo"
+    fi
+}
+
+ensure_installed dnf-plugins-core
+ensure_repo docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
+
 packages=(
+    containerd.io
+    docker-buildx-plugin
+    docker-ce
+    docker-ce-cli
+    docker-compose-plugin
     git-delta
     neovim
     powerline-fonts
     zeal
 )
 
-installed_packages=$(dnf list --installed)
-for pkg in "${packages[@]}"; do
-    if ! [[ "$installed_packages" =~ (^|$'\n')"$pkg"\. ]]; then
-        echo "Package $pkg not installed. Running bulk install...";
-        sudo dnf install -y "${packages[@]}"
-        break
-    fi
-done
+ensure_installed "${packages[@]}"
