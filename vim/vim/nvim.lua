90dc7653a5784560e820b8637af7910f8dc98a9d
John Starich <johnstarich@johnstarich.com>
2023-10-18 01:16:53 -0700

    vim: Use work-around for background color detection on Linux
    https://github.com/neovim/neovim/issues/17070

--- vim/vim/nvim.lua
+++ vim/vim/nvim.lua
@@ -1,3 +1,8 @@
+if vim.env.TMUX then
+    -- If running in tmux, detect background color. Remove after this is fixed: https://github.com/neovim/neovim/issues/17070#issuecomment-1767916121
+    vim.loop.fs_write(2, "\27Ptmux;\27\27]11;?\7\27\\", -1, nil)
+end
+
 -- ray-x/go.nvim
 require('go').setup {
     -- https://github.com/ray-x/go.nvim#configuration
