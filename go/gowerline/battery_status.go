b4047761a6f505f027ac9ab06e384d9742dc0634
John Starich <johnstarich@johnstarich.com>
2024-04-01 20:09:52 -0500

    gowerline: Read all battery entries under /sys/class/power_supply/BAT*

--- go/gowerline/battery_status.go
+++ go/gowerline/battery_status.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -9,28 +10,47 @@ import (
 	"strings"
 )
 
-func batteryStatus(_ context.Context, w io.Writer) error {
-	chargeNowBytes, err := os.ReadFile("/sys/class/power_supply/BAT1/charge_now")
+const powerSupplyFSPrefix = "/sys/class/power_supply/"
+
+func batteryStatus(ctx context.Context, w io.Writer) error {
+	batteryDirectories, err := findBatteryDirectories(ctx)
 	if err != nil {
 		return err
 	}
-	totalChargeBytes, err := os.ReadFile("/sys/class/power_supply/BAT1/charge_full")
-	if err != nil {
-		return err
+	if len(batteryDirectories) == 0 {
+		return errors.New("no battery detected")
 	}
-	chargeNow, err := strconv.ParseFloat(strings.TrimSpace(string(chargeNowBytes)), 64)
-	if err != nil {
-		return err
+
+	for index, batteryDir := range batteryDirectories {
+		if index > 0 {
+			fmt.Fprint(w, " ")
+		}
+		chargeNowBytes, err := os.ReadFile(batteryDir + "/charge_now")
+		if err != nil {
+			return err
+		}
+		totalChargeBytes, err := os.ReadFile(batteryDir + "/charge_full_design")
+		if err != nil {
+			return err
+		}
+		chargeNow, err := strconv.ParseFloat(strings.TrimSpace(string(chargeNowBytes)), 64)
+		if err != nil {
+			return err
+		}
+		totalCharge, err := strconv.ParseFloat(strings.TrimSpace(string(totalChargeBytes)), 64)
+		if err != nil {
+			return err
+		}
+		statusBytes, err := os.ReadFile(batteryDir + "/status")
+		if err != nil {
+			return err
+		}
+		chargePercent := chargeNow / totalCharge * 100
+		if chargePercent > 100 {
+			chargePercent = 100
+		}
+		fmt.Fprintf(w, "%s %.0f%%", batterySummaryForStatus(string(statusBytes)), chargePercent)
 	}
-	totalCharge, err := strconv.ParseFloat(strings.TrimSpace(string(totalChargeBytes)), 64)
-	if err != nil {
-		return err
-	}
-	statusBytes, err := os.ReadFile("/sys/class/power_supply/BAT1/status")
-	if err != nil {
-		return err
-	}
-	fmt.Fprintf(w, "%s %.0f%%", batterySummaryForStatus(string(statusBytes)), chargeNow/totalCharge*100)
 	return nil
 }
 
@@ -49,4 +69,18 @@ func batterySummaryForStatus(rawLinuxBatteryStatus string) string {
 	default:
 		return warning + " " + linuxBatteryStatus
 	}
+}
+
+func findBatteryDirectories(context.Context) ([]string, error) {
+	powerDirs, err := os.ReadDir(powerSupplyFSPrefix)
+	if err != nil {
+		return nil, err
+	}
+	var batteries []string
+	for _, dir := range powerDirs {
+		if strings.HasPrefix(dir.Name(), "BAT") {
+			batteries = append(batteries, powerSupplyFSPrefix+dir.Name())
+		}
+	}
+	return batteries, nil
 }
