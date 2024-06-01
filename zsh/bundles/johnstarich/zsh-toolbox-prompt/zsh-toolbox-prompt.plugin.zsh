b0cc4a439dbc2b2c52587ef3521af031cce11668
John Starich <johnstarich@johnstarich.com>
2024-06-01 15:25:23 -0500

    zsh: Add prompt segment for running in a Fedora toolbox

---
+++ zsh/bundles/johnstarich/zsh-toolbox-prompt/zsh-toolbox-prompt.plugin.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+autoload -Uz colors && colors
+
+js_toolbox_prompt=''
+if [[ -f /run/.containerenv && $(< /run/.containerenv) =~ 'name="([^"]*)"' ]]; then
+    local toolbox_name=$match
+    if [[ "$toolbox_name" == *toolbox* ]]; then
+        toolbox_name='🛠'
+    fi
+    local toolbox_color="%{$fg_bold[yellow]%}"
+    local reset_color="%{$reset_color%}"
+    if [[ -n "$toolbox_name" ]]; then
+        js_toolbox_prompt="${toolbox_color}${toolbox_name}${reset_color} "
+    fi
+fi
