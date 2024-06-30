1a56ba8d8e598c096a3b3ad31b76e7304646278c
John Starich <johnstarich@johnstarich.com>
2024-06-30 15:17:43 -0500

    gnome: Add systemd service to monitor and save power
    Depends on discharging and battery percentage

--- gnome/install.sh
+++ gnome/install.sh
@@ -6,3 +6,8 @@ fi
 
 dotlink applications ~/.local/share/applications/dotfiles
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
+
+for service in systemd/*; do
+    dotlink "$service" ~/.config/systemd/user/"$(basename "$service")"
+    systemctl --user enable --now power-monitor
+done
