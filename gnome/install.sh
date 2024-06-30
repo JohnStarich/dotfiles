b77eeab84e949373cad47bbb21ad473c6e257d65
John Starich <johnstarich@johnstarich.com>
2024-06-30 15:19:27 -0500

    gnome: Ensure ~/.local/systemd/user dir exists

--- gnome/install.sh
+++ gnome/install.sh
@@ -7,6 +7,7 @@ fi
 dotlink applications ~/.local/share/applications/dotfiles
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
 
+mkdir -p ~/.config/systemd/user
 for service in systemd/*; do
     dotlink "$service" ~/.config/systemd/user/"$(basename "$service")"
     systemctl --user enable --now power-monitor
