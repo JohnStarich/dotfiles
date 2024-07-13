ffe468a8ae9515e53ad94d48d0a95b835a0e7af3
John Starich <johnstarich@johnstarich.com>
2024-07-13 10:28:42 -0500

    gnome: Add autostart for Dropbox

--- gnome/install.sh
+++ gnome/install.sh
@@ -9,6 +9,7 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
 
 mkdir -p ~/.config/systemd/user
 for service in systemd/*; do
-    dotlink "$service" ~/.config/systemd/user/"$(basename "$service")"
-    systemctl --user enable --now power-monitor
+    name="$(basename "$service")"
+    dotlink "$service" ~/.config/systemd/user/"$name"
+    systemctl --user enable --now "$name"
 done
