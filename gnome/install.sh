29ced658928077d9b3b5e63506017fa702be404e
John Starich <johnstarich@johnstarich.com>
2024-08-12 22:38:47 -0500

    gnome: Disable high pitch beeping from calendar
    https://gitlab.gnome.org/GNOME/evolution/-/issues/1032

--- gnome/install.sh
+++ gnome/install.sh
@@ -6,6 +6,7 @@ fi
 
 dotlink applications ~/.local/share/applications/dotfiles
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"  # Set Caps Lock to press Escape instead
+gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/org-gnome-evolution-alarm-notify/ enable-sound-alerts false  # Disable high-pitch, annoying sounds for calendar alerts
 
 mkdir -p ~/.config/systemd/user
 for service in systemd/*; do
