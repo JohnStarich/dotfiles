e4caa4654758544dac41828e395e55d8a07572db
John Starich <johnstarich@johnstarich.com>
2025-06-17 20:30:30 -0500

    vim: Use latest bullets.vim repo name, prefer * bullet point, and allow indent/dedent with tab/shift+tab

--- vim/vim/nvim.lua
+++ vim/vim/nvim.lua
@@ -351,6 +351,8 @@ cmp.setup({
                 cmp.select_prev_item()
             elseif vim.fn["vsnip#jumpable"](-1) == 1 then
                 feedkey("<Plug>(vsnip-jump-prev)", "")
+            else
+                fallback()
             end
         end, { 'i', 's' }),
     }),
