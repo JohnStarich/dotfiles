b6e0a0fe0f31c72a11f9bedc95e073d068334706
John Starich <johnstarich@johnstarich.com>
2026-02-17 23:36:21 -0600

    gnome: Permit higher volume for S/PDIF audio out to balance with louder analog outs

--- gnome/install.sh
+++ gnome/install.sh
@@ -7,6 +7,7 @@ fi
 dotlink applications ~/.local/share/applications/dotfiles
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"  # Set Caps Lock to press Escape instead
 gsettings set org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/org-gnome-evolution-alarm-notify/ enable-sound-alerts false  # Disable high-pitch, annoying sounds for calendar alerts
+gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true  # Permit higher volume. Analog audio over USB doesn't behave well below 25%, but the S/PDIF audio was too quiet. This allows S/PDIF to reach similar audio levels.
 
 mkdir -p ~/.config/systemd/user
 for service in systemd/*; do
